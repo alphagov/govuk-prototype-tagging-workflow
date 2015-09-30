0e4407efb4998e6625486fa4ec094871159eb557
Alex Torrance <alex.torrance@digital.cabinet-office.gov.uk>
2015-09-30 09:44:50 +0100

    Include all the javascripts (from whitehall)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,19 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
+// the compiled file.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
+//= require vendor/jquery/jquery-ui-1.10.2.custom
+//= require vendor/jquery/jquery.player.min
+//= require vendor/jquery/magna-charta.min
+//= require vendor/sorttable
+//= require vendor/object-create-polyfill
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-//= require_tree ./admin
+//= require govuk_toolkit
+//= require shared_mustache
+//= require templates
+//
+//= require govuk
+//= require_tree ./common
+//= require_tree ./application
