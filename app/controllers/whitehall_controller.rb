0fec29140e7afce5f79402dacac1c7dea213b7a0
Alex Torrance <alex.torrance@digital.cabinet-office.gov.uk>
2015-10-01 13:56:52 +0100

    Break tagging out into its own page

--- app/controllers/whitehall_controller.rb
+++ app/controllers/whitehall_controller.rb
@@ -1,4 +1,10 @@
 class WhitehallController < ApplicationController
   def index
+
+  end
+
+  def tagging
+    @topics = JSON.parse(File.open('app/data/topics.json').read)['links']['children']
   end
 end
+
