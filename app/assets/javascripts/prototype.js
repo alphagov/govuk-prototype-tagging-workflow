897deb30889a7425eec0cf74144ba1475322ed81
Alex Torrance <alex.torrance@digital.cabinet-office.gov.uk>
2015-12-18 17:39:26 +0000

    Alternative tagging interface
    This presents two separate lists, one containing all the topics and subtopics and another with the selected topics.
    
    Topics are selected using a checkbox rather than clicking, this should hopefully make it more obvious when a topic is selected.
    
    The selected subtopics currently don’t show the topic, although this could be added.
    
    A typeahead find feature might also be useful.

--- app/assets/javascripts/prototype.js
+++ app/assets/javascripts/prototype.js
@@ -5,53 +5,79 @@ $(document).ready(function() {
     var $this = $(this);
     var $subtopic = $($this.attr('href'));
 
-    // hide other subtopics
-    $this.parents('.panel').find('.tag-explorer-topics-level-2').hide();
+    if ($this.hasClass('active')) {
+      $this.removeClass('active');
+      $this.siblings('.list-group-item').show();
 
-    // show selected subtopic
-    $subtopic.show();
-    $this.addClass('active').siblings().removeClass('active');
+      $subtopic.hide();
+    } else {
+      // hide other subtopics
+      $this.parents('.panel').find('.tag-explorer-topics-level-2').hide();
+
+      // show selected subtopic & hide other topics
+      $subtopic.show();
+      $this.siblings('.list-group-item').removeClass('active').hide();
+      $this.addClass('active');
+
+      // scroll to top
+      $this.parents('.tag-container').scrollTop(0);
+    }
   });
 
-  $('.tag-explorer-topics-level-2 a').on('click', function(e) {
-    e.preventDefault();
-
+  $('.tag-explorer-topics-level-2 input').on('change', function(e) {
     var $this = $(this);
-    var $chosen = $('#chosen-subtopics');
-    var val = $this.attr('href');
-    var $opt = $chosen.find('option[value='+val+']');
+    var $div = $this.parents('.list-group-item');
+    var $target = $('#selected-topics').find('.tag-container');
+    var $topic = $('.tag-explorer-topics-level-1 a:visible');
+    var val = $this.val();
 
-    if ($opt.attr('selected') == 'selected') {
-      $opt.attr('selected', false).trigger('chosen:updated');
+    if ($this.is(':checked') && $('#'+val+'-selected-item').length == 0) {
+      var $selected = $div.clone();
 
-      var topicSlug = $this.attr('id');
-      $('#topic-curated-warning-'+topicSlug).remove();
-
-      var $topicCuratedWarning = $('#topic-curated-warning');
-      if ($topicCuratedWarning.siblings().length == 0) {
-        $topicCuratedWarning.parent().remove();
-      }
-    } else {
-      $opt.attr('selected', true).trigger('chosen:updated');;
+      $selected.attr('id', val+'-selected-item');
+      $selected.appendTo($target);
+      $topic.addClass('list-group-item-info');
 
       // random 1 in 2
       if (Math.random() <= (1/2)) {
         var $topicCuratedWarning = $('#topic-curated-warning');
 
         if ($topicCuratedWarning.length == 0) {
+          var $warningTarget = $('#tag-explorer').parents('.row').first();
+
           $('<div class="notice bg-info"><p id="topic-curated-warning">Read the guidance on how to <a href="#">get content added to curated topics</a>.</p></div>')
-            .insertBefore('#tag-explorer');
+            .insertBefore($warningTarget);
           $topicCuratedWarning = $('#topic-curated-warning');
         }
 
-        var topicName = $this.text();
-        var topicSlug = $this.attr('id');
+        var topicName = $.trim($div.text());
+        $topicCuratedWarning.before('<p id="topic-curated-warning-'+val+'">'+topicName+' is a curated topic.</p>');
+      }
+    } else {
+      $('#'+val+'-selected-item').remove();
 
-        $topicCuratedWarning.before('<p id="topic-curated-warning-'+topicSlug+'">'+topicName+' is a curated topic.</p>');
+      if ($div.siblings().find('input:checked').length == 0) {
+        $topic.removeClass('list-group-item-info');
+      }
+
+      $('#topic-curated-warning-'+val).remove();
+
+      var $topicCuratedWarning = $('#topic-curated-warning');
+      if ($topicCuratedWarning.siblings().length == 0) {
+        $topicCuratedWarning.parent().remove();
       }
     }
+  });
 
-    $this.toggleClass('active');
+  $('#selected-topics').on('change', 'input', function(e) {
+    var $this = $(this);
+    var $div = $this.parents('.list-group-item');
+    var $master = $('#'+$this.val()+'-item');
+
+    if ($this.is(':checked') == false) {
+      $div.remove();
+      $master.find('input').click();
+    }
   });
 
   $('#chosen-subtopics').chosen().change(function(e, p) {
