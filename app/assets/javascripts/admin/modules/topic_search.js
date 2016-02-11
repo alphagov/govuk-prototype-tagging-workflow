0006c43d6caef529e6f847d2f46447ecdf42297b
Paul Hayes <fofr@users.noreply.github.com>
2016-02-11 12:35:35 +0000

    Add matching based on children

--- app/assets/javascripts/admin/modules/topic_search.js
+++ app/assets/javascripts/admin/modules/topic_search.js
@@ -6,6 +6,7 @@
 
       var parents = element.find('.js-topic-parent'),
           children = element.find('.js-topic-child'),
+          containers = element.find('.js-topic-parent-group'),
           tableInput = element.find('.js-topic-filter'),
           filterForm;
 
@@ -15,15 +16,44 @@
         var searchString = $.trim(tableInput.val()),
             regExp = new RegExp(escapeStringForRegexp(searchString), 'i');
 
+        // reset previous search matches
+        parents.data('child-match-count', 0);
+        children.data('matched', false);
+        containers.data('count', 0);
+
+        // search children, if match found, save count on group
+        children.each(function() {
+          var child = $(this),
+              container = child.parent('.js-topic-parent-group'),
+              count = container.data('count');
+
+          if (child.text().search(regExp) > -1) {
+            container.data('count', count + 1);
+            child.data('matched', true);
+          } else {
+            child.data('matched', false);
+          }
+        });
+
+        // search parents, hide or show based on match or children
         parents.each(function() {
-          var parent = $(this);
+          var parent = $(this),
+              childrenId = parent.attr('id').replace(/_parent/, ''),
+              $childrenContainer = $('#' + childrenId);
+
+          // Parent matched
           if (parent.text().search(regExp) > -1) {
             parent.show();
+
+          // Children matched
+          } else if ($childrenContainer.data('count') > 0) {
+            parent.show();
+
+          // Nothing matched
           } else {
             parent.hide();
           }
         });
-        console.log(searchString);
       }
 
       // http://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex
